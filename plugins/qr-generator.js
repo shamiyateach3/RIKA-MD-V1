8f56ddc4a26079cef2f3730aa43f574e83dbad91
Cyberrikado <rikadomd004@gmail.com>
2025-03-30 11:46:12 +0530

    Add files via upload

---
+++ plugins/qr-generator.js
@@ -0,0 +1,62 @@
+/*
+_  ______   _____ _____ _____ _   _
+| |/ / ___| |_   _| ____/___ | | | |
+| ' / |  _    | | |  _|| |   | |_| |
+| . \ |_| |   | | | |__| |___|  _  |
+|_|\_\____|   |_| |_____\____|_| |_|
+
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +237656520674
+YT: KermHackTools
+Github: Kgtech-cmr
+*/
+
+const moment = require("moment");
+const axios = require('axios');
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+cmd({
+  pattern: 'qrcode',
+  alias: ['qr'],
+  react: '🔄',
+  desc: 'Generate a QR code.',
+  category: 'main',
+  filename: __filename
+}, async (conn, mek, m, {
+  from,
+  quoted,
+  body,
+  isCmd,
+  command,
+  args,
+  q,
+  isGroup,
+  sender,
+  senderNumber,
+  botNumber2,
+  botNumber,
+  pushname,
+  isMe,
+  isOwner,
+  groupMetadata,
+  groupName,
+  participants,
+  groupAdmins,
+  isBotAdmins,
+  isAdmins,
+  reply
+}) => {
+  try {
+    if (!q) return reply('Please provide text to generate QR code.');
+    await reply('> *Rika Generating QR code...🔄*');
+    const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(q)}&size=200x200`;
+    const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
+    const buffer = Buffer.from(response.data, 'binary');
+    
+    await conn.sendMessage(m.chat, { image: buffer }, { quoted: m, caption: 'QR Code By Kerm Md' });
+  } catch (error) {
+    console.error(error);
+    reply(`An error occurred: ${error.message}`);
+  }
+});
